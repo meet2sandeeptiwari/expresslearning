68509ec47b63aab47463fb52d8a10a0604eada12
sandeeptiwari01 <meet2sandeeptiwari@gmail.com>
2026-07-11 22:14:32 +0530

    initial comit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("", (req,resp)=>{
     resp.send("<h1>Basic node js example</h1>")
 });
 app.get("/about", (req,resp)=>{
-    resp.send("<h1>This is about page</h1>")
+    resp.send("<h1>This is about page .</h1>")
 });
 
 app.get("/contact", (req,resp)=>{
