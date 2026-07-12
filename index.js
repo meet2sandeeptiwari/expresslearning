ffb14571146cd64beffc1c7ad09cdf5aa9aadaba
sandeeptiwari01 <meet2sandeeptiwari@gmail.com>
2026-07-12 14:57:03 +0530

    path module

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const express=require("express");
+import express from "express";
+const app = express();
 
-const app=express();
 
-app.get("", (req,resp)=>{
-    resp.send("<h1>Basic node js example</h1>")
-});
-app.get("/about", (req,resp)=>{
-    resp.send("<h1>This is about page</h1>")
-});
-
-app.get("/contact", (req,resp)=>{
-    resp.send("<h1>This is contact page</h1>")
+app.get("/", (req, resp) => {
+  resp.send("hello");
 });
 
 
