010397f234574a732ca1e898e2d9a6722bc3bfc3
sandeeptiwari01 <meet2sandeeptiwari@gmail.com>
2026-07-12 15:49:09 +0530

    added middleware

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import express from "express";
 const app = express();
 
+// function agecheck(req, resp, next){
+//     if(!req.query.age || req.query.age<18){
+//         resp.send("alert! you can not access this page");
+//     }else{
+//         next();
+//     }
+// }
+// app.use(agecheck);
+
+
+function ipcheck(req, resp, next){
+    const ip=req.socket.remoteAddress
+    console.log(ip);
+    if(!ip.includes('192.168.1.6')){
+        resp.send("alert! you can not access this page")
+    }else{
+        next();
+    }
+}
+app.use(ipcheck);
 
 app.get("/", (req, resp) => {
-  resp.send("hello");
+  resp.send("this is home page");
 });
 
+app.get("/login", (req, resp) => {
+  resp.send("this is login page");
+});
+
+app.get("/admin", (req, resp) => {
+  resp.send("this is admin page");
+});
 
 app.listen(3200);
